587ce17652cfdb75bbc49ac1a078200a22e747da
alden-flores <aldenjustineflores@gmail.com>
2024-12-15 10:20:30 +0800

    Revert "modifying user models"
    This reverts commit 44f6821192da190838e13c96985b4fa1c19b97bd.

--- studentportal/home/models.py
+++ studentportal/home/models.py
@@ -2,6 +2,38 @@ from django.db import models
 
 
 # Create your models here.
+class User(AbstractBaseUser):
+    
+    class Types(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        STUDENT = "STUDENT", "Student"
+        FACULTY = "FACULTY", "Faculty"
+        CASHIER = "CASHIER", "Cashier"
+        REGISTRAR = "REGISTRAR", "Registrar"
 
+    type = models.CharField(_('Type'), max_length=255, choices=Types.choices, default=Types.ADMIN)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    username = models.CharField(max_length=255,unique=True)
+    password = models.CharField(max_length=255, blank=False)
 
-# USER MODELS HERE
+    USERNAME_FIELD = 'username'
+
+    def get_absolute_url(self):
+        return reverse("users:detail",kwargs={"username": self.username})
+    
+class StudentTable(User):
+    class Meta:
+        proxy = True
+
+class CashierTable(User):
+    class Meta:
+        proxy = True
+
+class FacultyTable(User):
+    class Meta:
+        proxy = True
+
+class RegistrarTable(User):
+    class Meta:
+        proxy = True
+
