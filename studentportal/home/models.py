75a741b647182e55fdb045a4e50e70b1a971c5ff
alden-flores <aldenjustineflores@gmail.com>
2024-12-15 10:21:09 +0800

    ddd

--- studentportal/home/models.py
+++ studentportal/home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class User(AbstractBaseUser):
