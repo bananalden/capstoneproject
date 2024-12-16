24a51124febf3f0c1ada1d7dbce8eb9f5564942c
bananalden <128482606+bananalden@users.noreply.github.com>
2024-12-16 10:06:59 +0800

    new import for user

--- studentportal/home/models.py
+++ studentportal/home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-
+from django.db.models.signals import post_save
 
 # Create your models here.
 class User(AbstractUser):
