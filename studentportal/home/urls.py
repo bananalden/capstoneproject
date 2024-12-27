df2f22aaf06fea358a18d15658368c82741f4649
alden-flores <aldenjustineflores@gmail.com>
2024-12-27 21:24:20 +0800

    added comments to urls

--- studentportal/home/urls.py
+++ studentportal/home/urls.py
@@ -3,13 +3,25 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='login'),
+    #STUDENT URLS START
     path('studentdashboard', views.studentview, name='studentdashboard'),
+    #STUDENT URLS END
+
+    #REGISTRAR URLS START
     path('registrarview', views.registrarview, name='registrardashboard'),
+    #REGISTRAR URLS END
+
+    #CASHIER URLS START
     path('cashierview', views.cashierrview, name='cashierdashboard'),
+    #CASHIER URLS END
+
+
     path('facultyview', views.facultyview, name='facultydashboard'),
     path('moderator', views.moderatorview, name='moderatordashboard'),
     path('createuser', views.createuser, name='createuser'),
     path('accessdenied',views.accessdenied, name='accessdenied'),
-    path('logout', views.logout_user, name='logout'),
 
+    #MISC START
+    path('logout', views.logout_user, name='logout'),
+    #MISC END
 ]
