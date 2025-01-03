577c7d4e02b4d0f8715c1f232b2a6d3f93ad6ae8
alden-flores <aldenjustineflores@gmail.com>
2025-01-03 21:42:47 +0800

    defining saving methods

--- studentportal/users/signals.py
+++ studentportal/users/signals.py
@@ -5,5 +5,18 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender=Student)
 def create_student_profile (sender, instance, created, **kwargs):
-    if created:
-        StudentProfile.objects.create()
+    if created and instance.role =="STUDENT":
+        StudentProfile.objects.create(
+            student_id = instance
+        )
+
+@receiver(post_save, sender=Student)
+def save_student_profile (sender, instance, created, **kwargs):
+    instance.profile.save()
+
+@receiver(post_save, sender=Teacher)
+def create_teacher_profile (sender, instance, created, **kwargs):
+    if created and instance.role =="TEACHER":
+        TeacherProfile.objects.create(
+            teacher_id = instance
+        )
