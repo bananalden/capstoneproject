15b004b9efacc6d874d76f9f18cffab27cc85b0c
alden-flores <aldenjustineflores@gmail.com>
2025-01-04 12:56:17 +0800

    added error within authentication

--- studentportal/authentication/urls.py
+++ studentportal/authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from authentication import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('401',views.unauthorized_view, name='unauthorized-view')
 ]
--- studentportal/authentication/views.py
+++ studentportal/authentication/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'login/login.html')
+
+def unauthorized_view(request):
+    return render(request,'deniedaccess/404.html')
