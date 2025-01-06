7ec3fae4a781bd0be79766aaf620056f5050dde0
bananalden <128482606+bananalden@users.noreply.github.com>
2025-01-07 05:19:01 +0800

    added dynamic table viewing

--- studentportal/course/views.py
+++ studentportal/course/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 from course.forms import add_course
+from course.models import Course
 
 # Create your views here.
 def create_course(request):
+
     form = add_course()
+    course = Course.objects.all()
     if request.method == 'POST':
-        if form.is_valid():
-            form.save(request.POST)
-            return redirect('/schoolmanagement/course-list')
+        f = add_course(request.POST)
+        if f.is_valid():
+            f.save()
+            return redirect('course:course-list')
     else:
-        context = {'form' : form}
+        context = {'form' : form,
+                   'course': course
+                   }
         return render(request, 'createcourse.html', context)
+
+def update_course(request):
+    pass
 
 def create_semester(request):
     return render(request, 'createsemester.html')
