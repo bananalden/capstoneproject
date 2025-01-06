a8af474f4f1da2b047e7f92139c78cf004f070dd
alden-flores <aldenjustineflores@gmail.com>
2025-01-06 13:52:03 +0800

    added transaction models

---
+++ studentportal/transactions/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from users.models import Student
+
+# Create your models here.
+class Transaction(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", 'Pending'
+        PROCESSING = "PROCESSING", 'Processing'
+        AVAILABLE = "AVAILABLE", 'Available'
+
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    is_approved = models.BooleanField()
+    status = models.CharField(max_length=255, choices=Status.choices, default=Status.PENDING)
