f7afe87d04eaac6e9265b62751eeb59c2246efe1
alden-flores <aldenjustineflores@gmail.com>
2025-01-06 11:32:38 +0800

    added more crud tables

--- studentportal/admin/urls.py
+++ studentportal/admin/urls.py
@@ -3,5 +3,7 @@ from admin import views
 
 urlpatterns = [
     path('',views.home, name='admin-dashboard'),
-    path('course-list', views.create_course, name='create-course')
+    path('course-list', views.create_course, name='create-course'),
+    path('semester-list', views.create_semester, name='create-semester'),
+    path('subject-list', views.create_subject, name='create-subject'),
 ]
--- studentportal/admin/views.py
+++ studentportal/admin/views.py
@@ -7,3 +7,9 @@ def home(request):
 
 def create_course(request):
     return render(request, 'createcourse.html')
+
+def create_semester(request):
+    return render(request, 'createsemester.html')
+
+def create_subject(request):
+    return render(request, 'createsubject.html')
