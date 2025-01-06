e8a42eabc2885c595ee09161126178d5766a3451
bananalden <128482606+bananalden@users.noreply.github.com>
2025-01-06 21:32:13 +0800

    working on CRUD operations

---
+++ studentportal/course/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from course.models import Course
+
+class add_course(ModelForm):
+    class Meta:
+        model = Course
+        fields = ['name']
+        labels ={
+            "name":""
+        }
--- studentportal/course/views.py
+++ studentportal/course/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from course.forms import add_course
 
 # Create your views here.
 def create_course(request):
-    return render(request, 'createcourse.html')
+    form = add_course()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save(request.POST)
+            return redirect('/schoolmanagement/course-list')
+    else:
+        context = {'form' : form}
+        return render(request, 'createcourse.html', context)
 
 def create_semester(request):
     return render(request, 'createsemester.html')
