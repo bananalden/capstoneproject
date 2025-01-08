8192df1598b63bcaaad3d25cdaffb2548f4386d2
bananalden <128482606+bananalden@users.noreply.github.com>
2025-01-08 18:06:31 +0800

    added spacing to routes

--- studentportal/course/urls.py
+++ studentportal/course/urls.py
@@ -4,22 +4,27 @@ from course import views
 app_name = 'course'
 urlpatterns = [
 
-    #COURSE LIST CONTROLLERS START
+    #COURSE LIST ROUTES START
     path('course-list', views.create_course, name='course-list'),
     path('get-course-data/<int:pk>', views.get_coursedata, name='get_model_data'),
     path('course-list/edit', views.update_course, name='course-edit'),
     path('course-list/delete', views.delete_course, name='course-delete'),
-    #COURSE CONTROLLERS END
-    #SEMESTER LIST CONTROLLERS START
+    #COURSE ROUTES END
+
+    #SEMESTER LIST ROUTES START
     path('semester-list', views.create_semester, name='semester-list'),
     path('get-semester-data/<int:pk>', views.get_semesterdata, name='get_semester_data'),
     path('semester-list/edit', views.update_semester, name='semester-edit'),
     path('semester-list/delete', views.delete_semester, name='semester-delete'),
-    #SEMESTER LIST CONTROLLERS END
+    #SEMESTER LIST ROUTES END
+
+    #SUBJECT LIST ROUTES START
     path('subject-list', views.create_subject, name='subject-list'),
     path('get-subject-data/<int:pk>', views.get_subjectdata, name='get_subject_data'),
     path('subject-list/edit', views.update_subject, name='subject-edit'),
-        path('subject-list/delete', views.delete_subject, name='subject-delete'),
+    path('subject-list/delete', views.delete_subject, name='subject-delete'),
+    #SUBJECT LIST ROUTES END
+
     #APIS START
     path('api/courses', views.get_course_list, name='get-course-list')
     #APIS END
