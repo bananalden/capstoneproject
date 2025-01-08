2d5b34ed7f3b719b39f58c234ff2234c44695146
bananalden <128482606+bananalden@users.noreply.github.com>
2025-01-08 08:12:27 +0800

    fixed models

--- studentportal/course/models.py
+++ studentportal/course/models.py
@@ -11,20 +11,20 @@ class Semester(models.Model):
         SEM_1 = "SEM1",'1st Semester'
         SEM_2 = "SEM2", '2nd Semester'
     semester = models.CharField(choices=SemNumber.choices)
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     year = models.CharField(max_length=25, null=False,blank=False)
     def __str__(self):
         return self.semester
 
 class Subject(models.Model):
     name = models.CharField(max_length=255)
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    semester_id = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
 class Grades(models.Model):
-    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    semester_id = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     grade = models.FloatField(max_length=25)
