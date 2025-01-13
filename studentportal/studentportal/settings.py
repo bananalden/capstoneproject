ca6a7452a0b453669c22fc0b07a717f9c4d71e13
bananalden <128482606+bananalden@users.noreply.github.com>
2025-01-13 16:05:24 +0800

    changed dirs

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'studentportal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
