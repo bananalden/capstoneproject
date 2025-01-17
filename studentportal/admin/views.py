b4a63b1182ac1a3495105806db91a9ecb431a09f
bananalden <128482606+bananalden@users.noreply.github.com>
2025-01-17 17:51:13 +0800

    added admin dashboard functionality

--- studentportal/admin/views.py
+++ studentportal/admin/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from users import forms as user_forms
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'dashboard.html')
+    if request.method == 'POST':
+        if request.POST.get("user_type") == 'ADMIN':
+            form = user_forms.add_admin(request.POST)
+            if form.is_valid():
+                admin = form.save(commit=False)
+                admin.set_password(
+                    form.cleaned_data["password"]
+                )
+                admin.save()
+                messages.success(request,"Admin successfully created!")
+                return redirect('admin:users:admin-list')
+            else:
+                messages.warning(request,form.errors)
+                return redirect('admin:users:admin-list')
+            
+        elif request.POST.get("user_type") == 'REGISTRAR':
+            form = user_forms.add_registrar(request.POST)
+            if form.is_valid():
+                registrar = form.save(commit=False)
+                registrar.set_password(
+                    form.cleaned_data['password']
+                )
+                registrar.save()
+                messages.success(request,"Registrar successfully created!")
+                return redirect("admin:users:registrar-list")
+            else:
+                messages.warning(request,form.errors)
+                return redirect('admin:users:registrar-list')
+            
+        elif request.POST.get("user_type") == 'CASHIER':
+            form = user_forms.add_cashier(request.POST)
+            if form.is_valid():
+                cashier = form.save(commit=False)
+    else:
+        return render(request, 'dashboard.html')
 
