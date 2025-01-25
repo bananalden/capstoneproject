d6b41f2f7b179acd734266eefa9fdc03a90facbc
alden-flores <aldenjustineflores@gmail.com>
2025-01-25 19:46:46 +0800

    fixed base and merge

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'studentportal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'developmentstudentportal',
-        'USER': 'developmentstudentportal',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_EHdMVOhXbdD72HDRUA1',
+        'HOST': 'django-smis-database-django-smis.i.aivencloud.com',
+        'PORT': '11913'
     }
 }
 
