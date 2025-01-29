8257b195af7f0cc6b4ffec31c9bd7b858114f0b6
alden-flores <aldenjustineflores@gmail.com>
2025-01-29 09:24:09 +0800

    removed button

--- studentportal/admin/views.py
+++ studentportal/admin/views.py
@@ -6,10 +6,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
-
-
-
 @login_required(login_url='authentication:login')
 def home(request):
     return render(request, 'dashboard.html')
