34098e39d6ea4ab77c3ef0b28a2b6707dba325ca
alden-flores <aldenjustineflores@gmail.com>
2025-02-16 16:16:29 +0800

    datetime values finally fixed

--- studentportal/news/views.py
+++ studentportal/news/views.py
@@ -1,5 +1,7 @@
+from django.utils.timezone import now
 from django.shortcuts import render,redirect
 from news.models import Announcement
+import pytz
 
 # Create your views here.
 def create_announcements(request):
@@ -8,7 +10,10 @@ def create_announcements(request):
         body = request.POST.get("body")
 
         if title and body:
-            Announcement.objects.create(title=title, body=body, author=request.user)
+            manila_timezone = pytz.timezone('Asia/Manila')
+            now_ph = now().astimezone(manila_timezone)
+
+            Announcement.objects.create(title=title, body=body, author=request.user, created_on=now_ph, modified_on=now_ph)
             print("Succesfully created announcement")
             return redirect('home:teacher-newsfeed')
         else:
