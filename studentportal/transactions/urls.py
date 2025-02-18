b147a00fd52753b8205d733a5f458da32356ffcd
alden-flores <aldenjustineflores@gmail.com>
2025-02-18 22:20:15 +0800

    added delete purpose payment

--- studentportal/transactions/urls.py
+++ studentportal/transactions/urls.py
@@ -6,4 +6,5 @@ app_name='transactions'
 urlpatterns = [
         path('create-payment-purpose/',views.create_payment_purpose,name='create-payment-purpose'),
         path('edit-payment-purpose/',views.edit_payment_purpose,name='edit-payment-purpose'),
+        path('delete-payment-purpose/',views.delete_payment_purpose,name='delete-payment-purpose'),
 ]
