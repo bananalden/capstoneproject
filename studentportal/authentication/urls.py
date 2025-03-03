7cc2eeedeef9e4877d51c05bcae572a660d2221d
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2025-03-03 15:16:52 +0800

    New update Login Page

--- studentportal/authentication/urls.py
+++ studentportal/authentication/urls.py
@@ -8,9 +8,14 @@ urlpatterns = [
     path('', views.home, name='login'),
     path('logout',views.logout_user,name='logout'),
     path('401',views.unauthorized_view, name='unauthorized-view'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='forgot-password.html'), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_email.html'), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_success/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    # path('reset_password/', auth_views.PasswordResetView.as_view(template_name='forgot-password.html'), name="reset_password"),
+    # path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_email.html'), name="password_reset_done"),
+    # path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    # path('reset_success/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
+    # EMAIL SENT
+     path('email-sent/', views.email_sent, name='email-sent'),
+
+     path('set-new-pass/', views.set_new_pass, name='set-new-pass'),
 
 ]
