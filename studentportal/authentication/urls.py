df00405f280d76238a2fe5d99a2db0773b156f78
bananalden <128482606+bananalden@users.noreply.github.com>
2025-03-07 10:51:15 +0800

    cleaned up urls

--- studentportal/authentication/urls.py
+++ studentportal/authentication/urls.py
@@ -10,11 +10,14 @@ urlpatterns = [
     path('auth-admin-user/',views.login_admin, name='log-admin'),
     path('logout',views.logout_user,name='logout'),
     path('401',views.unauthorized_view, name='unauthorized-view'),
+
+    #PASSWORD RESET LINKS
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', email_template_name='registration/password_reset_email.html', success_url=reverse_lazy('authentication:password_reset_done')), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset.html'), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('authentication:password_reset_complete')), name="password_reset_confirm"),
     path('reset_success/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-
+    #PASSWORD RESET LINKS
+    
     # EMAIL SENT
      path('email-sent/', views.email_sent, name='email-sent'),
 
