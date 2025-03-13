b69ea0bc15892fa30447849c68190e089ec8bf35
alden-flores <aldenjustineflores@gmail.com>
2025-03-13 22:09:25 +0800

    modified models

--- studentportal/grades/models.py
+++ studentportal/grades/models.py
@@ -8,3 +8,4 @@ class Grades(models.Model):
     subject_name = models.CharField(max_length=255)
     semester = models.CharField(max_length=255)
     year = models.CharField(max_length=255)
+    grade_value = models.DecimalField(max_digits=5,decimal_places=2)
