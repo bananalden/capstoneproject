a88cd83f958dfea97ecca40b1d3384fd68e1624c
alden-flores <aldenjustineflores@gmail.com>
2025-03-13 14:39:45 +0800

    models for grades created

--- studentportal/grades/models.py
+++ studentportal/grades/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Grades(models.Model):
+    student_usn = models.CharField(max_length=255)
+    subject_code = models.CharField(max_length=255)
+    subject_name = models.CharField(max_length=255)
+    semester = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
