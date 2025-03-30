0f276ebd133de1b084eb9ca26ef49aee0865b336
alden-flores <aldenjustineflores@gmail.com>
2025-03-30 17:23:40 +0800

    grade updating works

--- studentportal/grades/forms.py
+++ studentportal/grades/forms.py
@@ -1,8 +1,21 @@
 from django import forms
 from grades.models import Grades
+from decimal import Decimal, InvalidOperation
 
 class edit_grade(forms.ModelForm):
 
     class Meta:
         model = Grades
         fields = ["grade_value"]
+
+    def clean_grade_value(self):
+        grade_value = self.cleaned_data.get('grade_value')
+        try:
+            grade_value = Decimal(str(grade_value))
+        except (InvalidOperation, TypeError):
+            raise forms.ValidationError("Invalid grade value. Please enter a valid number")
+        
+        if grade_value < 0 or grade_value > 100:
+            raise forms.ValidationError("Grade value must be between 0 and 100")
+        
+        return grade_value
