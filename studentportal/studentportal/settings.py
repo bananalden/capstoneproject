547647f01f0659b8abee67948a4f593a02e06f13
alden-flores <aldenjustineflores@gmail.com>
2025-05-05 19:12:42 +0800

    making secret key secret

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dr%fhn#7u^bes)^zw_fq&hqqq7#2(nnz-)j31&zis(^%0y%4g0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
