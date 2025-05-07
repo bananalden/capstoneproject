a3160a6b9a9acb7ca2d6f706b3a4dabc609283de
alden-flores <aldenjustineflores@gmail.com>
2025-05-07 18:42:09 +0800

    Changed views.py on uploading

--- studentportal/transactions/views.py
+++ studentportal/transactions/views.py
@@ -5,6 +5,7 @@ from transactions.forms import StudentPaymentForm,updatePayment,manualTransactio
 from transactions.models import Transaction
 from users.models import Student
 from django.contrib import messages
+from .imagekit_utils import upload_to_imagekit
 
 
 
@@ -14,7 +15,15 @@ def student_payment_request(request):
         form = StudentPaymentForm(request.POST, request.FILES)
 
         if form.is_valid():
-            form.save(user=request.user)
+            transaction = form.save(commit=False)
+            transaction.student = request.user
+            file = request.FILES.get('payment_proof')
+            if file:
+                image_url = upload_to_imagekit(file, file.name)
+                transaction.payment_proof = image_url
+
+            transaction.save()
+
             messages.success(request,"Payment request succesfully sent!")
             return redirect('home:student-request-form')
         else:
